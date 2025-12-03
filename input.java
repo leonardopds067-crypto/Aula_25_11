a9bf17d3cfc836050a44c0d4998a5d66101a9bf2
leonardopds067-crypto <leonardo.pds067@gmail.com>
2025-12-03 00:42:36 +0000

    aula do cd

---
+++ input.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Verificador da Cor do CD!");
+        System.out.println();
+        System.out.print("Insira a cor do CD desejado: ");
+        String cd = sc.nextline();
+        if (cd.equal("Azul")){
+            System.out.println("Esse CD custa R$10,00!");
+        } else{
+            if ()
+        }
+        }
+       
+    
+        
+
+}
+
+    
