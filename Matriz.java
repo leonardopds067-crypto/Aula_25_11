ff06ca07af244d715eec25d791f9954174c8ba6b
leonardopds067-crypto <leonardo.pds067@gmail.com>
2026-01-22 00:57:52 +0000

    Matriz.java

--- Matriz.java
+++ Matriz.java
@@ -4,6 +4,9 @@ public class Matriz {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int somatoria = 0;
+        if (linha == coluna) {
+            somatoriaDiagonal = somatoriaDiagonal + matriz[linha][coluna];
+        }
 
         // perguntar o user
         System.out.println("Sua Matriz tem quantas linhas? ");
@@ -13,6 +16,7 @@ public class Matriz {
         int tamanhoColuna = scanner.nextInt();
         
         int[][] matriz = new int[tamanhoLinha][tamanhoColuna];
+
 
         //Preenchendo a matriz
         System.out.println("Preencha a matriz: ");
@@ -28,10 +32,13 @@ public class Matriz {
              for (int coluna = 0; coluna < tamanhoColuna; coluna++) {
                 System.out.print(matriz[linha][coluna] + "  ");
                 somatoria = somatoria + matriz[linha][coluna];
+                somatoriaDiagonal =somatoriaDiagonal + matriz[i][i];
              }
              System.out.println(); // Nova linha após cada linha matriz
         }
         System.out.print("\n\nSOMATORIA:" + somatoria);
+        System.out.print("\nSOMATORIA:" + somatoriaDiagonal);
+
 
         scanner.close();
 
